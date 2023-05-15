5277a050ae33023e50a40e0671ce63627a8e0644
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2023-05-15 11:03:47 -0700

    add hi and hi2

---
+++ hi2.py
@@ -0,0 +1,20 @@
+ 
+# This script showcases some features of Python
+
+# Variables and data types
+name = "John"
+age = 25
+is_student = True
+height = 1.75
+
+# Printing variables
+print("My name is", name)
+print("I am", age, "years old")
+print("I am a student:", is_student)
+print("My height is", height, "meters")
+
+# Lists
+fruits = ["apple", "banana", "orange"]
+print("My favorite fruit is", fruits[0])
+print("I also like", fruits[1:], "and", fruits[-1])
+
---
+++ test-git-subtree-2/hi.py
@@ -0,0 +1,8 @@
+# Python script showcasing some features of Python
+
+# 1. List comprehension
+squares = [x**2 for x in range(10)]
+print("List comprehension:", squares)
+
+# 2. Dictionary comprehension
+squares_dict = {x: x**2 for x in range(10)}
